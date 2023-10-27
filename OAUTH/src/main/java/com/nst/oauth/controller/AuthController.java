adf44bf0571d8f5357bd028511a43c7930c7081f
yukhan-kishore <yukhan.kishore@newstreettech.com>
2023-10-27 12:43:45 +0530

    added wso2 auth route

--- OAUTH/src/main/java/com/nst/oauth/controller/AuthController.java
+++ OAUTH/src/main/java/com/nst/oauth/controller/AuthController.java
@@ -1,13 +1,30 @@
 package com.nst.oauth.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.nst.oauth.config.TokenGenerator;
 import com.nst.oauth.dto.ErrorDTO;
 import com.nst.oauth.dto.LoginDTO;
 import com.nst.oauth.dto.ResponseDTO;
+import com.nst.oauth.dto.TokenDTO;
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.URIScheme;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
@@ -17,7 +34,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -32,6 +58,12 @@ public class AuthController {
     @Qualifier("jwtAccessTokenAuthProvider")
     JwtAuthenticationProvider accessTokenAuthProvider;
 
+
+    @Value("${wso2-token-url}")
+    String wso2TokenURL;
+
+    @Value("${wso2-client-token}")
+    String wso2Token;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginDTO loginDTO) {
@@ -65,5 +97,45 @@ public class AuthController {
             return new ResponseEntity<>(result,HttpStatus.OK);
 
         }
+    }
+
+    @PostMapping("/getWso2Token")
+    public ResponseEntity getToken() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, JsonProcessingException {
+
+//  ------------------- TO MAKE REQ WITH CERT ---------
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial((X509Certificate[] certificateChain, String authType) -> true)  // <--- accepts each certificate
+                .build();
+
+        Registry<ConnectionSocketFactory> socketRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register(URIScheme.HTTPS.getId(), new SSLConnectionSocketFactory(sslContext))
+                .register(URIScheme.HTTP.getId(), new PlainConnectionSocketFactory())
+                .build();
+
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setConnectionManager(new PoolingHttpClientConnectionManager(socketRegistry))
+                .setConnectionManagerShared(true)
+                .build();
+
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + wso2Token);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> bodyParamMap = new HashMap<>();
+        bodyParamMap.put("grant_type","client_credentials");
+
+        String reqBodyData = new ObjectMapper().writeValueAsString(bodyParamMap);
+        HttpEntity<String> requestEnty = new HttpEntity<>(reqBodyData, headers);
+        ResponseEntity<String> exchange = restTemplate.postForEntity(wso2TokenURL, requestEnty, String.class);
+        JsonObject resultJsonObject = new JsonParser().parse(exchange.getBody()).getAsJsonObject();
+        TokenDTO accessToken = TokenDTO.builder().accessToken(resultJsonObject.get("access_token").getAsString()).build();
+        ResponseDTO<Object> result = ResponseDTO.builder()
+                .status(Boolean.TRUE)
+                .result(accessToken)
+                .build();
+        return new ResponseEntity<>(result,HttpStatus.OK);
     }
 }
--- OAUTH/src/main/java/com/nst/oauth/dto/TokenDTO.java
+++ OAUTH/src/main/java/com/nst/oauth/dto/TokenDTO.java
@@ -1,14 +1,14 @@
 package com.nst.oauth.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TokenDTO {
     private String userId;
     private String accessToken;
